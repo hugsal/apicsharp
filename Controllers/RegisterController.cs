903da47c679b5fd70bc66be7fe92e9cce29dfeb5
Hugs <hugs.salazar@gmail.com>
2021-08-18 21:35:39 -0500

    first commit

---
+++ Controllers/RegisterController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apiExercise.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RegisterController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Ok");
+        }
+
+        [HttpPost("add")]
+        public IActionResult AddPerson(Register user)
+        {
+            Console.WriteLine(user);
+            if (user.UserName == "") return BadRequest("User is empty");
+            if (user.Password == "") return BadRequest("Password is empty");
+            if (user.Password != user.ConfPassword) return BadRequest("password do not match");
+            return CreatedAtAction(nameof(AddPerson), "User add succefully");
+        }
+    }
+}
---
+++ Models/Register.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace apiExercise
+{
+    public class Register
+    {
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public string ConfPassword { get; set; }
+
+    }
+}
